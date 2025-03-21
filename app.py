20293f63ba6d72fcebf41d5ad801bcdb9c496c6e
SurinLi <LI0032IN@e.ntu.edu.sg>
2025-03-21 12:42:42 +0000

    1

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
 from flask import Flask, request, render_template
+import sqlite3
+import datetime
+import google.generativeai as genai
+import os
+
+api = "AIzaSyAs2sDGGjawWbDEgHV6O3l8x8WQH6cbg0A"
+model = genai.GenerativeModel("gemini-1.5-flash")
+genai.configure(api_key=api)
 
 app = Flask(__name__)
+
+flag = 1
 
 @app.route("/", methods=["POST", "GET"])
 def index():
@@ -8,7 +18,17 @@ def index():
 
 @app.route("/main", methods=['GET', 'POST'])
 def main():
-    user_name = request.form.get("q")
+    global flag
+    if flag == 1:
+        t = datetime.datetime.now()
+        user_name = request.form.get("q")
+        conn = sqlite3.connect('user.db')
+        c = conn.cursor()
+        c.execute("insert into user (name, timestamp) values (?,?)", (user_name, t))
+        conn.commit()
+        c.close()
+        conn.close
+        flag = 0
     return(render_template("main.html"))
 
 @app.route("/foodexp", methods=['GET', 'POST'])
@@ -31,7 +51,38 @@ def test_result():
         return(render_template("pass.html"))
     elif answer =="true":
         return(render_template("fail.html"))
-    
+
+@app.route("/FAQ", methods=['GET', 'POST'])
+def FAQ():
+    return(render_template("FAQ.html"))
+
+@app.route("/FAQ1", methods=['GET', 'POST'])
+def FAQ1():
+    r = model.generate_content("Factors for Profit")
+    return(render_template("FAQ1.html", r=r.candidates[0].content.parts[0]))
+
+@app.route("/userLog", methods=['GET', 'POST'])
+def userLog():
+    conn = sqlite3.connect('user.db')
+    c = conn.cursor()
+    c.execute("select * from user")
+    r = ""
+    for row in c:
+        r = r + str(row) + "\n"
+    print(r)
+    c.close()
+    conn.close
+    return(render_template("userLog.html", r=r))   
+
+@app.route("/deleteLog", methods=['GET', 'POST'])
+def deleteLog():
+    conn = sqlite3.connect('user.db')
+    c = conn.cursor()
+    c.execute("delete from user")
+    conn.commit()
+    c.close()
+    conn.close
+    return(render_template("deleteLog.html"))
 
 if __name__ == "__main__":
     app.run()
